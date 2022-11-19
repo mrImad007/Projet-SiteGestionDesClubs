881f13042d9d1340c04be506c05dc5add076274c
Imad Eddine Zaoui <imadeddinzaoui@gmail.com>
2022-11-19 16:57:06 +0100

    New version PHP

---
+++ php/index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/home.css">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Open+Sans:ital@1&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+
+        <!-- start navigation -->
+        <div class="nav">
+            <div class="logo">
+                <a href="index.php" title="Recode your future">
+                    <img src="logo.png" alt="logo">
+                </a>
+            </div>
+            <div class="menu">
+                <!-- <a href="index.html">Home</a> -->
+                <i class="uil uil-signin"></i>
+            </div>
+        </div>
+        <div class="sign-box">
+            <div class="signinform">
+                <h3>Sign in</h3>
+                <p>Log in to access your management account on Youcode Clubs</p>
+            <form action="gestionClub.php" method="POST">
+            <div class="inputboite">
+                  <input type="text" name="name" placeholder="Username" />
+                </div>
+                <div class="inputboite">
+                  <input type="password" placeholder="Password" name="password"/>
+                </div>
+                <div class="inputboite connect-btn">
+                  <input class="connect" type="submit" value="Connect" />
+                </div>
+              </div>
+            </form>
+        </div>
+    </header>
+    <!-- end navigation -->
+    <div class="contenu">
+            <!-- start content -->
+    <h1>La vie estudiantine</h1>
+
+    <div class="intro">
+        <h2>La dynamique associative des élèves</h2>
+        <div class="text">
+            <p>La formation à l’Ecole Youcode va au-delà du cadre strictement scolaire, les initiatives du travail associatif sont vivement encouragées. La vie associative vient pour compléter les études, Elle permet aux élèves de forger leur personnalité en développant davantage le sens de contact et le travail en équipe dont le maître mot est : agissons ensemble.</p>
+            <p>Youcode considère la participation à la vie associative comme un complément indispensable de son projet éducatif. Dans cette perspective, un bouquet d’association a vu le jour permettant considérablement aux élèves de l’Ecole de rompre avec le système traditionnel et de développer ainsi un esprit communautaire et associatif.</p>
+        </div>
+        <div class="ttl">
+            <h2>Listes des clubs de l’école</h2>
+        </div>
+    </div>
+
+    <div class="container">
+        <!-- club 1 -->
+        <div class="club">
+            <div class="cont">
+                <div class="img">
+                    <img src="greenInvest.jpg" alt="#">
+                </div>
+                <div class="description">
+                    <h3>Club des Arts et Métiers</h3>
+                    <h4>Presentation</h4>
+                    <p>L’objectif l’association Sportive YouCode est de participer aux différentes activités parascolaires à l'école et il a un  plan d’action bien défini qui s’inscrit dans une démarche globale d’éducation au sport qui vient s’allier à la vocation pédagogique de l’école.</p>
+                    <p>YouCode dispose des équipements pour toutes les disciplines sportives masculines et féminines. la chose qui permet de bien veiller à l’organisation de ses activités et à participer aux différents championnats universitaires régionaux et nationaux.</p>
+                    <h4>Missions</h4>
+                    <ul class="tasks">
+                        <li>Organiser tout au long de l’année des événements sur différents thèmes sociaux au profit de différents bénéficiaires</li>
+                        <li>Participer à des événements de marque à travers ses délégations d’étudiants, pour une simple assistance ou une implication dans l’organisation</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- club 2 -->
+        <div class="club">
+            <div class="cont">
+                <div class="img">
+                    <img src="greenInvest.jpg" alt="#">
+                </div>
+                <div class="description">
+                    <h3>l'Association sportive Youcode</h3>
+                    <h4>Presentation</h4>
+                    <p>l'Association des Arts et Métiers (ADAM) est une association à but non lucratif à vocation artistique qui a pour objet l'encadrement et le perfectionnement des activités artistiques, elle siège à YouCode. </p>
+                    <p>L'Association des arts et métiers est composée d'ateliers qui sont à la disposition des étudiants de l'école : Ateliers musique et chant, danse, design, théâtre, photographie, infographie….
+                        C'est ainsi que l'ADAM offre à ses membres l'opportunité de joindre l'utile à l'agréable, en accompagnant ses artistes en herbe dans leur épanouissement artistique, et en offrant aux futurs managers la possibilité de mettre en pratique leur acquis.</p>
+                    <h4>Missions</h4>
+                    <ul class="tasks">
+                        <li>Organiser tout au long de l’année des événements sur différents thèmes sociaux au profit de différents bénéficiaires</li>
+                        <li>Participer à des événements de marque à travers ses délégations d’étudiants, pour une simple assistance ou une implication dans l’organisation</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- club 3 -->
+        <div class="club">
+            <div class="cont">
+                <div class="img">
+                    <img src="greenInvest.jpg" alt="#">
+                </div>
+                <div class="description">
+                    <h3>Club culture</h3>
+                    <h4>Presentation</h4>
+                    <p>Appelé aussi le CDA, le Club de promotion et de Parrainage a été créé en 2022. Il est parmi les meilleurs clubs à chapeauter le réseau de l'ecole au travers le parascolaire dont elle dispose. C’est le club qui se voit confier le drapeau de l’école.</p>
+                    <p>Le  est le club qui vous a accueilli aux différents forums nationaux et internationaux tout au long de l’année en représentant l’école Youcode, bien avant que vous sachiez quel chemin vous allez suivre après l’obtention de votre baccalauréat.</p>
+                    <h4>Missions</h4>
+                    <ul class="tasks">
+                        <li>Organiser tout au long de l’année des événements sur différents thèmes sociaux au profit de différents bénéficiaires</li>
+                        <li>Participer à des événements de marque à travers ses délégations d’étudiants, pour une simple assistance ou une implication dans l’organisation</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+        <!--/.footer-->
+    <footer id="footer"  class="footer">
+        <div class="container">
+            <div class="hm-footer-copyright text-center" style="width:1500px ;">
+                <div class="footer-social">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                </div>
+                <p style="font-family:Inter ;">
+                    &copy;copyright. designed and developed by <a href="https://www.linkedin.com/in/imad-eddine-zaoui-a2ab95246/"><span style="color: rgb(65, 166, 255);">ZAOUI Imad Eddine</span></a>
+                </p>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+    <script type="text/javascript" src="./js.js"></script>
+</body>
+</html>
---
+++ php/js.js
@@ -0,0 +1,11 @@
+
+const signBtn = document.querySelector(".uil");
+const menuSign = document.querySelector(".signinform");
+
+let contenu = document.querySelector(".sign-box");
+
+
+signBtn.onclick = () => {
+    contenu.classList.toggle("active-box");
+};
+console.log(signBtn.onclick)
