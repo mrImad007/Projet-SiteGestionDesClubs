1d082ac85c6c440c1e1ffdbfe53703bf651f87c9
Imad Eddine Zaoui <imadeddinzaoui@gmail.com>
2022-11-21 09:58:41 +0100

    Changes

--- php/addClub.php
+++ php/addClub.php
@@ -1,24 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'><link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Open+Sans:ital@1&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/ClubGes.css">
+    <title>gestion des clubs</title>
+</head>
+<body>
 <?php
-$name = $_POST["clubName"];
-$desc = $_POST["clubDesc"];
-$mission = $_POST["clubMiss"];
-$category = $_POST["clubCatg"];
+                $name = $_POST["clubName"];
+                $desc = $_POST["clubDesc"];
+                $mission = $_POST["clubMiss"];
+                $category = $_POST["clubCatg"];
+            
+                $conn = new PDO("mysql:host=localhost;dbname=brief2;port=3306",'root','');
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$conn = new PDO("mysql:host=localhost;dbname=brief2;port=3306",'root','');
-
-$ins = "INSERT INTO `clubs` (`Name`, `Description`, `Mission`, `image`, `id`, `categorie`) VALUES ('?','?','?','?','?','?')";
-$del = "DELETE FROM `clubs` WHERE   "
-$exe = $conn->prepare($ins);
-$exe->execute();
-
-<div class="add" action="<?php echo $_SERVER['PHP_SELF'] ?>"method="post"">
-                <form action="#" method="post">
-                    <input type="text" name="clubName" placeholder="Nom du club"><br><br>
-                    <input type="text" name="clubDesc" placeholder="description"><br><br>
-                    <input type="text" name="clubMiss" placeholder="mission"><br><br>
-                    <input type="text" name="clubCatg" placeholder="category"><br><br>
-                    <input type="file" id="img" name="img" accept="image/*"><br><br>
-                </form>
-            </div>
-
+                $ins = "INSERT INTO `clubs` (`Name`, `Description`, `Mission`, `image`, `id`, `categorie`) VALUES ('?','?','?','?','?','?')";
+                $exe = $conn->prepare($ins);
+                $exe->execute();
 ?>
+                <div class="forms" action="<?php echo $_SERVER['PHP_SELF'] ?> "method="post">
+                    <form method="post">
+                        <input type="text" name="clubName" placeholder="Nom du club"><br><br>
+                        <input type="text" name="clubDesc" placeholder="description"><br><br>
+                        <input type="text" name="clubMiss" placeholder="mission"><br><br>
+                        <input type="text" name="clubCatg" placeholder="category"><br><br>
+                        <input type="file" id="img" name="img" accept="image/*"><br><br>
+                    </form>
+                </div>
+</body>
+</html>
